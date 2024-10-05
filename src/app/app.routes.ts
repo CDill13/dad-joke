c0eec383ac80d1698b75358dcee0b503384e42aa
CDill13 <cody.dillman13@gmail.com>
2024-10-05 16:26:22 -0600

    styling and move search to page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { JokePageComponent } from './components/joke-page/joke-page.component';
 import { FavoritesPageComponent } from './components/favorites-page/favorites-page.component';
+import { JokeSearchComponent } from './components/joke-search/joke-search.component';
 
 export const routes: Routes = [
   { path: 'joke-page/:jokeId', component: JokePageComponent },
@@ -12,4 +13,5 @@ export const routes: Routes = [
   },
   { path: 'home', component: HomeComponent },
   { path: 'favorites', component: FavoritesPageComponent },
+  { path: 'search', component: JokeSearchComponent },
 ];
