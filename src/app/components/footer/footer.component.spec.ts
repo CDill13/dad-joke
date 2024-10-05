eacc7ef1599cfac934d9a00f5f209d9b554fa0f2
CDill13 <cody.dillman13@gmail.com>
2024-10-05 15:06:07 -0600

    Update footer

--- src/app/components/footer/footer.component.spec.ts
+++ src/app/components/footer/footer.component.spec.ts
@@ -20,4 +20,11 @@ describe('FooterComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should set signals correctly', () => {
+    expect(component.codyLinkedIn()).toEqual('https://www.linkedin.com/in/cody-dillman/');
+    expect(component.linkedInImage()).toEqual('https://i.pinimg.com/originals/86/32/54/863254f8c36146219554977cc345d584.png');
+    expect(component.codyGitHub()).toEqual('https://github.com/CDill13');
+    expect(component.gitHubImage()).toEqual('https://cdn-icons-png.flaticon.com/512/25/25231.png');
+  })
 });
--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 
 @Component({
   selector: 'app-footer',
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss',
 })
-export class FooterComponent {}
+export class FooterComponent {
+  public codyLinkedIn = signal('https://www.linkedin.com/in/cody-dillman/');
+  public linkedInImage = signal('https://i.pinimg.com/originals/86/32/54/863254f8c36146219554977cc345d584.png');
+  public codyGitHub = signal('https://github.com/CDill13');
+  public gitHubImage = signal('https://cdn-icons-png.flaticon.com/512/25/25231.png');
+}
