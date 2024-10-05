8195c33679509de3c7bd19bf48ecfc3af23d5c43
CDill13 <cody.dillman13@gmail.com>
2024-10-05 14:54:32 -0600

    Update app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 
@@ -10,6 +11,4 @@ import { FooterComponent } from './components/footer/footer.component';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent {
-  title = 'dad-joke';
-}
+export class AppComponent {}
