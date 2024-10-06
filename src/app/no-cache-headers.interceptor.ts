09729ffdb3a02cb3a9d01a197fbc6ad66e7231a7
CDill13 <cody.dillman13@gmail.com>
2024-10-05 23:25:30 -0600

    Add return type

--- src/app/no-cache-headers.interceptor.ts
+++ src/app/no-cache-headers.interceptor.ts
@@ -1,9 +1,10 @@
-import { HttpHandler, HttpInterceptor, HttpRequest } from "@angular/common/http";
+import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from "@angular/common/http";
 import { Injectable } from "@angular/core";
+import { Observable } from "rxjs";
 
 @Injectable()
 export class NoCacheHeadersInterceptor implements HttpInterceptor {
-  intercept(req: HttpRequest<any>, next: HttpHandler) {
+  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<unknown>> {
     const authReq = req.clone({
       setHeaders: {
         'Cache-Control': 'no-cache',
